c290051ea28d879aba6a57ad7740cb31b8dc3ff8
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-22 15:24:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Coordinator Dashboard",
+    page_icon="🏥",
+    layout="wide"
+)
+
+st.title("🏥 Ward Coordinator Monitoring Dashboard")
+
+st.success("Application Started Successfully")
+
+st.write("Welcome to Coordinator Monitoring Portal")
