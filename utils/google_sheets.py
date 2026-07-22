1f23ba49ac44836f12c0802c0dd4bfc42fcf3743
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-22 16:20:43 +0530

    Update google_sheets.py

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,1 +1,57 @@
 
+import requests
+
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzEz6BVmAWhoR5Wws5h2FU76pH6ypE-aMe4bKopvRPHVuAWqI7Gfa5w4Je7i4YvJemk/exec"
+
+
+def save_review(
+    review_date,
+    coordinator,
+    task,
+    frequency,
+    priority,
+    status,
+    remarks
+):
+
+    payload = {
+
+        "date": str(review_date),
+
+        "coordinator": coordinator,
+
+        "task": task,
+
+        "frequency": frequency,
+
+        "priority": priority,
+
+        "status": status,
+
+        "remarks": remarks
+
+    }
+
+    try:
+
+        response = requests.post(
+
+            WEB_APP_URL,
+
+            json=payload,
+
+            timeout=30
+
+        )
+
+        return response.json()
+
+    except Exception as e:
+
+        return {
+
+            "success": False,
+
+            "message": str(e)
+
+        }
