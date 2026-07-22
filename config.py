a920e2b8489b22f2dacbf1e20b513ec3e899d458
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-22 15:36:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+APP_NAME = "Ward Coordinator Monitoring Dashboard"
+
+APP_YEAR = "2026"
+
+COORDINATORS = [
+    "Coordinator 1",
+    "Coordinator 2",
+    "Coordinator 3",
+    "Coordinator 4"
+]
