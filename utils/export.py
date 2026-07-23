9aa89ba5b7ada6972126f2f55957c37316fa9b10
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-23 15:12:43 +0530

    Update export.py

--- utils/export.py
+++ utils/export.py
@@ -1,1 +1,17 @@
+import io
+import pandas as pd
 
+
+def dataframe_to_excel(df):
+
+    output = io.BytesIO()
+
+    with pd.ExcelWriter(output, engine="openpyxl") as writer:
+
+        df.to_excel(
+            writer,
+            index=False,
+            sheet_name="Report"
+        )
+
+    return output.getvalue()
