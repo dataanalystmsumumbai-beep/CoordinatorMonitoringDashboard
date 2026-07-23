ec5289fc5e7829686ff598741895a42214148f95
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-23 16:02:47 +0530

    Update export.py

--- utils/export.py
+++ utils/export.py
@@ -2,16 +2,34 @@ import io
 import pandas as pd
 
 
-def dataframe_to_excel(df):
+def export_excel(df):
 
     output = io.BytesIO()
 
-    with pd.ExcelWriter(output, engine="openpyxl") as writer:
+    with pd.ExcelWriter(
+        output,
+        engine="openpyxl"
+    ) as writer:
 
         df.to_excel(
             writer,
             index=False,
             sheet_name="Report"
         )
+
+        ws = writer.sheets["Report"]
+
+        for col in ws.columns:
+
+            length = max(
+                len(str(cell.value))
+                if cell.value
+                else 0
+                for cell in col
+            )
+
+            ws.column_dimensions[
+                col[0].column_letter
+            ].width = length + 4
 
     return output.getvalue()
