edef487d1ce668271cf7eb93b42332857ae0a54a
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-23 16:15:59 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,68 @@
+import plotly.express as px
 
+
+def pie_chart(df):
+
+    fig = px.pie(
+
+        df,
+
+        names="Status",
+
+        values="Count",
+
+        hole=.60
+
+    )
+
+    fig.update_layout(
+        height=400
+    )
+
+    return fig
+
+
+def coordinator_chart(df):
+
+    fig = px.bar(
+
+        df,
+
+        x="coordinator",
+
+        y="Completed",
+
+        text="Completed",
+
+        color="Completed"
+
+    )
+
+    fig.update_layout(
+        height=400
+    )
+
+    return fig
+
+
+def trend_chart(df):
+
+    fig = px.line(
+
+        df,
+
+        x="date",
+
+        y="Count",
+
+        color="status",
+
+        markers=True
+
+    )
+
+    fig.update_layout(
+        height=450
+    )
+
+    return fig
