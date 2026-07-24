c519def91ca6cb868281bdf23478a5bc6de01b0e
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-24 12:54:36 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,24 @@
+from datetime import datetime
 
+
+def today():
+
+    return datetime.today().strftime("%d-%m-%Y")
+
+
+def current_time():
+
+    return datetime.now().strftime("%I:%M %p")
+
+
+def greeting():
+
+    hour = datetime.now().hour
+
+    if hour < 12:
+        return "Good Morning"
+
+    elif hour < 17:
+        return "Good Afternoon"
+
+    return "Good Evening"
