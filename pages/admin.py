6718e7eb107ca712628c635ff228095f817e5f82
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-24 19:06:45 +0530

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -15,8 +15,33 @@ tab1, tab2 = st.tabs([
 ])
 
 with tab1:
-    st.subheader("Coordinator Management")
-    st.info("Coming in next step...")
+
+    st.subheader("👤 Coordinator Management")
+
+    name = st.text_input(
+        "Coordinator Name"
+    )
+
+    if st.button(
+        "➕ Add Coordinator",
+        use_container_width=True
+    ):
+
+        if name.strip() == "":
+
+            st.warning("Enter Coordinator Name")
+
+        else:
+
+            result = add_coordinator(name)
+
+            if result["success"]:
+
+                st.success("Coordinator Added Successfully")
+
+            else:
+
+                st.error(result)
 
 with tab2:
     st.subheader("Master Task Management")
